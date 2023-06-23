04b0ea59f9de147b8c9ffbf53a153acba13f2865
realtimechris <40668522+RealTimeChris@users.noreply.github.com>
2023-06-23 14:25:02 +0000

    [bot] VCPKG info update [skip ci]

--- Vcpkg/ports/jsonifier/portfile.cmake
+++ Vcpkg/ports/jsonifier/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO realtimechris/jsonifier
     REF "v${VERSION}"
-    SHA512 05f6c1ad02f758bd2796e821cb1c46eca9e1b898e16f85ba77ccb1c5d8573c0e727f5a616731d6bf222efac9027413f7fef0c29f2bad16703a90e14de38ffa22
+    SHA512 7373bd8d8554f9b76eb80d1c6b906d917ede80d55f5c363c6c26c67b4e659d9e657fb15d2da7d1eaf41fc24ee84fd54b16d8a0bd9bd76aacf899990eee425845
     HEAD_REF main
 )
 
