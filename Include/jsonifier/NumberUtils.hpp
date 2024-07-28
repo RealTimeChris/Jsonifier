cf950a79d51854695d333a336fc46c40d10efade
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2024-07-27 23:45:37 -0400

    Adding fast_float implementation.

--- Include/jsonifier/NumberUtils.hpp
+++ Include/jsonifier/NumberUtils.hpp
@@ -58,7 +58,9 @@ namespace jsonifier {
 		double newValue{};
 		if (string.size() > 0) [[likely]] {
 			auto currentIter = reinterpret_cast<const char*>(string.data());
-			jsonifier_internal::parseFloat(newValue, currentIter);
+			auto endIter	 = reinterpret_cast<const char*>(string.data() + string.size());
+			static constexpr jsonifier_internal::parse_options options{ jsonifier_internal::chars_format::json };
+			auto [ptr, ec] = jsonifier_internal::from_chars_advanced(currentIter, endIter, newValue, options);
 		}
 		return newValue;
 	}
@@ -205,15 +207,16 @@ namespace jsonifier_internal {
 				}
 			}
 		} else {
-			auto s = parseFloat(value, newPtr);
-			++iter;
-			if (!s) [[unlikely]] {
+			static constexpr jsonifier_internal::parse_options optionsNew{ jsonifier_internal::chars_format::json };
+			auto [ptr, ec] = jsonifier_internal::from_chars_advanced(static_cast<const char*>(iter), static_cast<const char*>(end), value, optionsNew);
+			if (ec != std::errc()) [[unlikely]] {
 				static constexpr auto sourceLocation{ std::source_location::current() };
 				errors.emplace_back(error::constructError<sourceLocation, error_classes::Parsing, parse_errors::Invalid_Number_Value>(iter - options.rootIter,
 					end - options.rootIter, options.rootIter));
 				skipToNextValue(iter, end);
 				return;
 			}
+			++iter;
 		}
 	}
 
@@ -310,14 +313,16 @@ namespace jsonifier_internal {
 				}
 			}
 		} else {
-			auto s = parseFloat(value, iter);
-			if (!s) [[unlikely]] {
+			static constexpr jsonifier_internal::parse_options optionsNew{ jsonifier_internal::chars_format::json };
+			auto [ptr, ec] = jsonifier_internal::from_chars_advanced(iter, end, value, optionsNew);
+			if (ec != std::errc()) [[unlikely]] {
 				static constexpr auto sourceLocation{ std::source_location::current() };
 				errors.emplace_back(error::constructError<sourceLocation, error_classes::Parsing, parse_errors::Invalid_Number_Value>(iter - options.rootIter,
 					end - options.rootIter, options.rootIter));
 				skipToNextValue(iter, end);
 				return;
 			}
+			iter = ptr;
 		}
 	}
 
