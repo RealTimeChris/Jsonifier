whisprer/rtc-jsonifier

Jsonifier Release v0.9.97
* Added likely/unlikely to a bunch of if/else statements.
* Swapped std::copy_n for std::memcpy in a few places.
* Moved a couple masks for the Neon functions out into global scope.
* Replaced jsonifier_internal::const_iterator with jsonifier_internal::iterator.
* Moved the vector of prettifier states out of the function scope to the prettifier classes' scope.
* Separated jsonifier_internal::string_literal out into its own header.
* Moved a bunch of string-utility functions out into the derailleur class.
* Replaced the join function with combineLiterals.
* Modified the logic of the parse_impl classes.
* Implemented a compile-time jump table to improve parsing performance significantly.
* Replaced the float/double parsing/serializing methods with more conformant version.
* Modified the serializing code to no longer rely on hash-maps for serializing member.
* Marked a bunch of additional functions as noexcept.
* Added an ALWAYS_INLINE macro in addition to the INLINE one.
* Modified the CPU-architecture selection method.

Include/jsonifier/ISA/Bmi.hpp
@@ -23,21 +23,15 @@
 /// Feb 3, 2023
 #pragma once
 
-#include <jsonifier/Config.hpp>
+#include <jsonifier/TypeEntities.hpp>
 
 namespace simd_internal {
 
 #if JSONIFIER_CHECK_FOR_INSTRUCTION(JSONIFIER_BMI) || JSONIFIER_CHECK_FOR_INSTRUCTION(JSONIFIER_ANY_AVX)
 
-	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_INLINE value_type blsr(value_type value) {
-		return _blsr_u32(value);
-	}
+	#define blsr(value) _blsr_u64(value)
 
-	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_INLINE value_type blsr(value_type value) {
-		return _blsr_u64(value);
-	}
-
-	template<jsonifier::concepts::uint16_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint16_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) noexcept {
 	#if defined(JSONIFIER_LINUX)
 		return __tzcnt_u16(value);
 	#else
@@ -45,21 +39,19 @@ namespace simd_internal {
 	#endif
 	}
 
-	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) noexcept {
 		return _tzcnt_u32(value);
 	}
 
-	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) noexcept {
 		return _tzcnt_u64(value);
 	}
 
 #elif JSONIFIER_CHECK_FOR_INSTRUCTION(JSONIFIER_NEON)
 
-	template<jsonifier::concepts::unsigned_type value_type> JSONIFIER_INLINE value_type blsr(value_type value) {
-		return value & (value - 1);
-	}
+	#define blsr(value) (value & (value - 1))
 
-	template<jsonifier::concepts::uint16_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint16_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) noexcept {
 	#if JSONIFIER_REGULAR_VISUAL_STUDIO
 		return _tzcnt_u16(value);
 	#else
@@ -67,7 +59,7 @@ namespace simd_internal {
 	#endif
 	}
 
-	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) noexcept {
 	#if JSONIFIER_REGULAR_VISUAL_STUDIO
 		return _tzcnt_u32(value);
 	#else
@@ -75,7 +67,7 @@ namespace simd_internal {
 	#endif
 	}
 
-	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) noexcept {
 	#if JSONIFIER_REGULAR_VISUAL_STUDIO
 		return _tzcnt_u64(value);
 	#else
@@ -85,15 +77,9 @@ namespace simd_internal {
 
 #else
 
-	template<jsonifier::concepts::unsigned_type value_type> JSONIFIER_INLINE value_type blsr(value_type value) {
-		if (value == 0) {
-			return 0;
-		}
+	#define blsr(value) (value & (value - 1))
 
-		return value & (value - 1);
-	}
-
-	template<jsonifier::concepts::unsigned_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::unsigned_type value_type> JSONIFIER_ALWAYS_INLINE value_type tzcnt(value_type value) noexcept {
 		if (value == 0) {
 			return sizeof(value_type) * 8;
 		}

RealTimeChris/Jsonifier

Include/jsonifier/ISA/Bmi.hpp
@@ -1,7 +1,7 @@
 /*
 	MIT License
 
-	Copyright (c) 2023 RealTimeChris
+	Copyright (c) 2024 RealTimeChris
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy of this
 	software and associated documentation files (the "Software"), to deal in the Software
@@ -23,21 +23,15 @@
 /// Feb 3, 2023
 #pragma once
 
-#include <jsonifier/Config.hpp>
+#include <jsonifier/TypeEntities.hpp>
 
 namespace simd_internal {
 
 #if JSONIFIER_CHECK_FOR_INSTRUCTION(JSONIFIER_BMI) || JSONIFIER_CHECK_FOR_INSTRUCTION(JSONIFIER_ANY_AVX)
 
-	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_INLINE value_type blsr(value_type value) {
-		return _blsr_u32(value);
-	}
+	#define blsr(value) _blsr_u64(value)
 
-	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_INLINE value_type blsr(value_type value) {
-		return _blsr_u64(value);
-	}
-
-	template<jsonifier::concepts::uint16_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint16_type value_type> JSONIFIER_ALWAYS_INLINE value_type tzcnt(value_type value) noexcept {
 	#if defined(JSONIFIER_LINUX)
 		return __tzcnt_u16(value);
 	#else
@@ -45,21 +39,19 @@ namespace simd_internal {
 	#endif
 	}
 
-	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_ALWAYS_INLINE value_type tzcnt(value_type value) noexcept {
 		return _tzcnt_u32(value);
 	}
 
-	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_ALWAYS_INLINE value_type tzcnt(value_type value) noexcept {
 		return _tzcnt_u64(value);
 	}
 
 #elif JSONIFIER_CHECK_FOR_INSTRUCTION(JSONIFIER_NEON)
 
-	template<jsonifier::concepts::unsigned_type value_type> JSONIFIER_INLINE value_type blsr(value_type value) {
-		return value & (value - 1);
-	}
+	#define blsr(value) (value & (value - 1))
 
-	template<jsonifier::concepts::uint16_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint16_type value_type> JSONIFIER_ALWAYS_INLINE value_type tzcnt(value_type value) noexcept {
 	#if JSONIFIER_REGULAR_VISUAL_STUDIO
 		return _tzcnt_u16(value);
 	#else
@@ -67,7 +59,7 @@ namespace simd_internal {
 	#endif
 	}
 
-	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint32_type value_type> JSONIFIER_ALWAYS_INLINE value_type tzcnt(value_type value) noexcept {
 	#if JSONIFIER_REGULAR_VISUAL_STUDIO
 		return _tzcnt_u32(value);
 	#else
@@ -75,7 +67,7 @@ namespace simd_internal {
 	#endif
 	}
 
-	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::uint64_type value_type> JSONIFIER_ALWAYS_INLINE value_type tzcnt(value_type value) noexcept {
 	#if JSONIFIER_REGULAR_VISUAL_STUDIO
 		return _tzcnt_u64(value);
 	#else
@@ -85,15 +77,9 @@ namespace simd_internal {
 
 #else
 
-	template<jsonifier::concepts::unsigned_type value_type> JSONIFIER_INLINE value_type blsr(value_type value) {
-		if (value == 0) {
-			return 0;
-		}
+	#define blsr(value) (value & (value - 1))
 
-		return value & (value - 1);
-	}
-
-	template<jsonifier::concepts::unsigned_type value_type> JSONIFIER_INLINE value_type tzcnt(value_type value) {
+	template<jsonifier::concepts::unsigned_type value_type> JSONIFIER_ALWAYS_INLINE value_type tzcnt(value_type value) noexcept {
 		if (value == 0) {
 			return sizeof(value_type) * 8;
 		}